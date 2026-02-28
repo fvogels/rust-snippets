3f4290ce3ebb2c6f574304004be9ff3ef1ffa561
Frederic Vogels <frederic.vogels@gmail.com>
2026-02-28 17:58:34 +0100

    ModelState

--- src/ui/state.rs
+++ src/ui/state.rs
@@ -1,4 +1,4 @@
-use std::mem;
+use std::{collections::HashSet, mem};
 
 use ratatui::{Frame, crossterm::event::{Event}};
 
@@ -53,3 +53,46 @@ impl State {
         }
     }
 }
+
+pub struct ModelState {
+    library: Library,
+    selected_tags: Vec<String>,
+    keywords: Vec<String>,
+    visible_snippets: Vec<usize>,
+    visible_tags: Vec<String>,
+}
+
+impl ModelState {
+    pub fn add_keyword(&mut self, keyword: String) {
+        self.keywords.push(keyword);
+    }
+
+    pub fn select_tag(&mut self, tag: String) {
+        self.selected_tags.push(tag);
+        self.update_visible_snippets();
+        self.update_visible_tags();
+    }
+
+    fn update_visible_snippets(&mut self) {
+        self.visible_snippets = self.library.search(self.keywords.iter().map(String::as_str), self.selected_tags.iter().map(String::as_str));
+    }
+
+    fn update_visible_tags(&mut self) {
+        let mut tags: HashSet<String> = HashSet::new();
+
+        for snippet_id in self.visible_snippets.iter().copied() {
+            let snippet = self.library.snippet(snippet_id);
+            let snippet_tags = &snippet.tags;
+
+            tags.retain(|tag| snippet_tags.contains(tag));
+        }
+
+        for selected_tag in self.selected_tags.iter() {
+            tags.remove(selected_tag);
+        }
+
+        self.visible_tags = tags.into_iter().collect();
+        self.visible_tags.sort();
+    }
+}
+
