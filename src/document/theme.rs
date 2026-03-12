9258c1806309764888c088095dfbbb79626b0da5
Frederic Vogels <frederic.vogels@gmail.com>
2026-03-12 12:40:04 +0100

    headings in theme

--- src/document/theme.rs
+++ src/document/theme.rs
@@ -3,6 +3,7 @@ use crate::document::{Color, Style};
 pub struct Theme {
     pub default: Style,
     pub inline_code: Style,
+    pub headings: [Style; 3]
 }
 
 impl Theme {
@@ -10,6 +11,11 @@ impl Theme {
         Theme{
             default: Style::default(),
             inline_code: Style::default().background(Color::gray(128)),
+            headings: [
+                Style::default().background(Color::blue(128)).bold(),
+                Style::default().background(Color::blue(64)),
+                Style::default().bold(),
+            ]
         }
     }
 }
